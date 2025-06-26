e9804e2e3392b97c8b03794d18981a09be34fd50
karpanitava <startstop191@gmail.com>
2025-06-26 12:01:51 +0530

    Create exam.groovy

---
+++ vars/exam.groovy
@@ -0,0 +1,12 @@
+def  sum(a,b){
+  echo "The Sum of ${a} and ${b} is {a+b}"
+}
+def  sub(a,b){
+  echo "The  subrtaction of ${a} and ${b} is {a-b}"
+}
+def  mul(a,b){
+  echo "The product of ${a} and ${b} is {a*b}"
+}
+def  div(a,b){
+  echo "The division of ${a} and ${b} is {a/b}"
+}
